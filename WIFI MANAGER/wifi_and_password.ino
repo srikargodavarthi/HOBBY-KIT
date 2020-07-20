8fb6a24d093d9a478a4513f6f044edf82010b804
srikargodavarthi <63180451+srikargodavarthi@users.noreply.github.com>
2020-07-20 14:25:25 +0530

    Add files via upload

---
+++ WIFI MANAGER/wifi_and_password.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+////// include libraris hear ////////
+
+
+void setup() {
+Serial.begin(115200);
+WiFiManager wifiManager;
+wifiManager.autoConnect("Hobbykit ","");
+
+
+
+//enter your setup//
+
+
+
+
+}
+void loop(){
+
+
+//enter your loop//
+  
+}
